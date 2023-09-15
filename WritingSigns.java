9502e54ae98c5739b11cdacc2c0e5ed97e7332e5
Вероника <144523735+VeronikaYamancheva@users.noreply.github.com>
2023-09-15 23:44:59 +0300

    Add files via upload

---
+++ WritingSigns.java
@@ -0,0 +1,11 @@
+package FirstHomework;
+
+//Задание 2.3. Вывод знаков
+public class WritingSigns {
+    public static void main(String[] args) {
+        System.out.println("+++");
+        System.out.println("!!!!");
+        System.out.println("??");
+
+    }
+}
